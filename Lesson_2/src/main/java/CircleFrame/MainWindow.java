092a33d44a5b3a337dc13e7c66a9a04cf9a989a3
Sergei <sergeygrubber@gmail.com>
2024-07-13 15:14:00 +0300

    Lesson 2 (Balls)

---
+++ Lesson_2/src/main/java/CircleFrame/MainWindow.java
@@ -0,0 +1,50 @@
+package CircleFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+    private static final int POS_X = 400;
+    private static final int POS_Y = 200;
+    private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 600;
+    private final Sprite[] sprites = new Sprite[16];
+
+
+    private MainWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
+        setTitle("Circles");
+
+        MainCanvas canvas = new MainCanvas(this);
+        add(canvas);
+
+        setVisible(true);
+
+        for (int i = 0; i < sprites.length; i++) {
+            sprites[i] = new Ball();
+        }
+    }
+
+    public void onDrawFrame(MainCanvas canvas, Graphics g, float deltaTime) {
+        update(canvas, deltaTime);
+        render(canvas, g);
+    }
+
+    private void update(MainCanvas canvas, float deltaTime) {
+        for (int i = 0; i < sprites.length; i++) {
+            sprites[i].update(canvas, deltaTime);
+        }
+
+    }
+
+    private void render(MainCanvas canvas, Graphics g) {
+        for (int i = 0; i < sprites.length; i++) {
+            sprites[i].render(canvas, g);
+        }
+    }
+
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+}
